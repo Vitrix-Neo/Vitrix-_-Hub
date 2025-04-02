e702145af7c2803e6e959294e50b9a2c7c9fcb9e
Vitrix <lioontop701@gmail.com>
2025-04-02 22:36:21 +0200

    Update and rename .lua to 67528273z7363876373.lua

--- .lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/30d5d231b691d0a16b34d6b0.lua'))()
---
+++ 67528273z7363876373.lua
@@ -0,0 +1,2 @@
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/bff08177c7abfe52f5a6e3da.lua'))()
